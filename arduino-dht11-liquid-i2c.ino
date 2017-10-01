fcee34dd2daa119644b941f8ff92241017887ef2
Giovanni Win10 Giorgi <jj@gioorgi.com>
2017-10-01 11:36:32 +0200

    Rev 2 with sensor to detect prossimity and turn on light

--- arduino-dht11-liquid-i2c.ino
+++ arduino-dht11-liquid-i2c.ino
@@ -1,7 +1,16 @@
+// -*- mode:c++; mode: flymake -*-
 #include <Wire.h>
 
 #include <LiquidCrystal_I2C.h>
 #include <SimpleDHT.h>
+#include <Ultrasonic.h>
+/**
+ * See README.md for wiring information parts and so on
+ * 
+ * HC RS04 ultrasonic sensor is used to detect presence and turn on oww backlite
+ */
+Ultrasonic ultrasonic(11 /*TRIG*/ , 10 /*ECHO*/, 40000UL /*TIMEOUT*/);
+
 
 LiquidCrystal_I2C lcd(0x3F,20,4); // set the LCD address to 0x27 for a 20 chars and 4 line display
 
@@ -13,7 +22,7 @@ SimpleDHT11 dht11;
 
 void readTemperature(){
   static int readCounter=0;
-  int pinDHT11 = 2; // pin for dht sensor
+  int pinDHT11 = 13; // pin for dht sensor
     // start working...
   Serial.println("=================================");
   Serial.println("Sample DHT11...");
@@ -21,28 +30,24 @@ void readTemperature(){
   // read without samples.
   byte temperature = 0;
   byte humidity = 0;
-  int err = SimpleDHTErrSuccess;
-  if ((err = dht11.read(pinDHT11, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+  int err = dht11.read(pinDHT11, &temperature, &humidity, NULL);
+  if (err  != SimpleDHTErrSuccess) {
     Serial.print("Read DHT11 failed, err="); Serial.println(err);
     lcd.clear();
     lcd.setCursor(0,0);
-    lcd.print("err="); lcd.println((int)err);
+    lcd.print("Sensor err="); lcd.println((int)err);
     delay(900);
     return;
   }
   
-  Serial.print("Sample OK: ");
-  Serial.print((int)temperature); Serial.print(" *C, "); 
-  Serial.print((int)humidity); Serial.println(" H");
 
   // Push it to lcd
   lcd.clear();
   // set the cursor to column 0, line 0
   lcd.setCursor(0, 0);
   lcd.print((int)temperature);
-  lcd.print(" C ");
- 
-  lcd.setCursor(0, 1);
+  lcd.print(" C -- "); 
+  //lcd.setCursor(0, 1);
   lcd.print((int)humidity);
   lcd.print(" % Humyd");
 
@@ -65,11 +70,35 @@ lcd.backlight();
 }
 
 void loop(){
+  delay(1000); // Sensor init take some time
+  lcd.backlight();
   lcd.setCursor(0,2);
   lcd.print(" Scan...");
   readTemperature();
-  lcd.setCursor(0,2);
-  lcd.print(" by Giovanni Giorgi");
-  delay(70000);   // 70 sec
+
+  unsigned long startTime=millis();
+  /** To count time it is tricky:
+   * we do stuff until 70 sec are passed
+   */
+  while( millis() < (startTime+70000) ) {
+    lcd.setCursor(0, 1);
+    lcd.print("Time:");
+    lcd.print(startTime);
+    
+    unsigned int distCM=ultrasonic.distanceRead();  
+    lcd.setCursor(0,2);
+    lcd.print("... Dist:");
+    lcd.print((unsigned int)distCM);
+    lcd.print(" cm ");
+    if(distCM <=26 ) {
+      lcd.backlight();
+      delay(35000); // Let it look for a while
+    }else{
+      lcd.noBacklight();
+      delay(1300);
+    }
+
+  }
+
 }
 
